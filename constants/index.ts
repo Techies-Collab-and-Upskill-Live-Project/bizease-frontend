9e4e2392921278bd3358c6e941838a2f825450ad
FSA-22 <simeonstat@gmail.com>
2025-05-14 11:14:32 +0000

    feat(folder): add navItems with , name & icon paths

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+  { id: 1, name: 'Dashboard', iconPath: '/icon/home.svg', route: '/' },
+  { id: 2, name: 'Orders', iconPath: '/icon/cart.svg', route: '/oders' },
+  { id: 3, name: 'Tasks', iconPath: '/icon/task.svg', route: '/tasks' },
+  { id: 4, name: 'Report', iconPath: '/icon/report.svg', route: '/reports' },
+  { id: 5, name: 'Profile', iconPath: '/icon/profile.svg', route: '/profiles' },
+];
