0193959e596bea61d41f2345b04c688534946302
edem8 <eddieblack159@gmail.com>
2025-05-16 18:56:32 +0000

    feat: added onboarding ui import onboarding in default route app/pages.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
+import Onboarding from './onboarding';
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 font-bold text-4xl text-blue-700">
-      <h1>Welcome to Bizease</h1>
-      <p>Your one-stop solution for business management.</p>
-    </div>
+   <Onboarding />
   );
 };
 export default Home;
