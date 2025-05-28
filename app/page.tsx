50b1ed88107cc5ae0cad3b61858bac08a7d2ce74
FSA-22 <simeonstat@gmail.com>
2025-05-28 21:36:07 +0000

    fix:adjust  get started button width for responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Landing() {
             </p>
           </div>
           <img src="favicon.ico" alt="logo" className="my-6 h-20 w-20" />
-          <div className="px-5 py-2 md:px-20 md:py-4 lg:py-10 w-full lg:w-2/3">
+          <div className="px-5 py-2 md:px-20 md:py-4 max-sm:w-sm lg:py-10 w-xl lg:w-2/3">
             <Button
               onClick={handleGetStarted}
               className="rounded-lg text-base font-semibold bg-[#06005B] hover:bg-blue-900 cursor-pointer w-full py-6 md:py-8 lg:py-6"
