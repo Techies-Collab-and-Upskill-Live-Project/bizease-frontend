ba20d06ef9d44cd6cf102dfe856f22ac5e7d5108
FSA-22 <simeonstat@gmail.com>
2025-05-30 07:32:33 +0000

    fix: package conflict fix

--- app/(main)/inventory/page.tsx
+++ app/(main)/inventory/page.tsx
@@ -4,7 +4,7 @@ import TopAvatar from '@/components/navigations/TopAvatar';
 
 const Inventory = () => {
   return (
-    <section className="h-screen w-full bg-gray-100 overflow-scroll">
+    <section className="h-screen w-full bg-gray-100">
       <TopAvatar type="Inventory" />
       <div className="flex flex-col gap-2 min-h-screen w-full px-6 py-2">
         <h2 className="font-bold text-lg max-md:hidden">Inventory</h2>
