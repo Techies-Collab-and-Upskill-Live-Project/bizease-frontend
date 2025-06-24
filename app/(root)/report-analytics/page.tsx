f13c41d711a6fb382339241c61f2da8b3d6bb6f1
FSA-22 <simeonstat@gmail.com>
2025-06-24 22:28:27 +0000

    feat: implement download file report export page

--- app/(root)/report-analytics/page.tsx
+++ app/(root)/report-analytics/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 
 import TopAvatar from '@/components/navigations/TopAvatar';
@@ -8,7 +10,7 @@ import Charts from '@/components/reports/Charts';
 
 const ReportAnalytics = () => {
   return (
-    <section className="w-full min-h-screen bg-gray-100 pt-20 px-4 md:px-8 lg:px-12">
+    <section className="w-full min-h-screen bg-gray-100 pt-20 px-4 md:px-8 lg:px-12 overflow-x-hidden">
       {/* Fixed Top Navigation */}
       <div className="fixed top-0 left-0 right-0 z-50 bg-surface-100 shadow-md">
         <TopAvatar type="Reports" />
@@ -16,12 +18,17 @@ const ReportAnalytics = () => {
 
       {/* Content Layout */}
       <div className="w-full mx-auto space-y-8 mt-6">
+        {/* Selects and Period Filter */}
         <HeaderPeriodSelection />
-        <div className="overflow-x-auto">
+
+        {/* KPI Sums and Charts */}
+        <div className="w-full overflow-x-visible">
           <ReportSums />
           <Charts />
         </div>
-        <div className="overflow-x-auto">
+
+        {/* Table Summary */}
+        <div className="w-full overflow-x-visible">
           <SummaryTable />
         </div>
       </div>
