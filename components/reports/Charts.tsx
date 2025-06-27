0615a078a02c1933fee56ec8954a22d8eda23823
FSA-22 <simeonstat@gmail.com>
2025-06-27 12:52:23 +0000

    fix barchart dynamic revenue data

--- components/reports/Charts.tsx
+++ components/reports/Charts.tsx
@@ -58,14 +58,27 @@ const Charts = () => {
     'rgba(34,211,238,0.4)',
   ];
 
+  // const barChartData = {
+  //   labels: topProducts.map(({ name }) => name),
+  //   datasets: [
+  //     {
+  //       label: 'Revenue',
+  //       data: topProducts.map((p) => p),
+  //       backgroundColor: topProducts.map((_, i) => colors[i % colors.length]),
+  //       borderRadius: 4,
+  //     },
+  //   ],
+  // };
+
   const barChartData = {
     labels: topProducts.map(({ name }) => name),
     datasets: [
       {
         label: 'Revenue',
-        data: topProducts.map((p) => p),
+        data: topProducts.map(({ revenue }) => revenue),
         backgroundColor: topProducts.map((_, i) => colors[i % colors.length]),
-        borderRadius: 4,
+        borderRadius: 6,
+        borderSkipped: false,
       },
     ],
   };
@@ -118,7 +131,7 @@ const Charts = () => {
               <CustomLegend label="Order" />
             </div>
             <Line
-              style={{ width: '100%', maxWidth: '100%' }}
+              style={{ width: '98%', maxWidth: '98%' }}
               data={lineChartData}
               options={chartOptions}
             />
