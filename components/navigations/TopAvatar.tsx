08e0f60406533754cc3bc20713c25a8f72464c73
FSA-22 <simeonstat@gmail.com>
2025-08-15 14:50:13 +0000

    fix: change mobile logout button bg to lighter shade of blue

--- components/navigations/TopAvatar.tsx
+++ components/navigations/TopAvatar.tsx
@@ -73,7 +73,7 @@ const TopAvatar = ({ type }: CustomLogoProp) => {
             <div className="absolute -right-2 top-7.5 mt-2 w-45 bg-gray-100 py-8 px-2 shadow-lg rounded-lg max-lg:block hidden">
               <button
                 onClick={handleAllLogout}
-                className="block w-full text-center px-4 py-2 bg-darkblue rounded-2xl font-medium text-gray-100 hover:text-gray-100 hover:bg-lightblue "
+                className="block w-full text-center text-darkblue px-4 py-2 rounded-2xl font-semibold bg-darkblue/10  hover:text-gray-100 hover:bg-lightblue "
               >
                 Log Out
               </button>
